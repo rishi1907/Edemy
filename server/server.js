c4ce484c4618151195bbfa453c75d45bef207baa
rishi1907 <risikeshranjan074@gmail.com>
2025-04-28 01:22:46 +0530

    backend commit

--- server/server.js
+++ server/server.js
@@ -1,15 +1,21 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './configs/mongodb.js'
+import { clerkWebhook } from './controllers/webhooks.js'
 
 //Initialise Express
 const app = express()
+
+//connect to database
+await connectDB()
 
 // Middlewares
 app.use(cors())
 
 // Routes
 app.get('/', (req, res) => res.send("API Working"))
+app.post('/clerk', express.json(), clerkWebhook)
 
 // Port
 const PORT = process.env.PORT || 5000
